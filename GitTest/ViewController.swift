a8ca09627fcc0ef181dc3955c65529a5359682b4
Kristiaan <kris.swinton@gmail.com>
2018-04-13 21:35:33 -0700

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Kristiaan Swinton on 4/13/18.
+//  Copyright © 2018 Kristiaan Swinton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
